090b936481403ecdc20dd22e608d4e6500113f03
Christian <ctrouse@ucsc.edu>
2021-04-16 00:07:47 -0700

    Rocket Patrol sfxRocket hotfix

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,8 +1,9 @@
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
         super(scene, x, y, texture, frame);
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
+
 
         // add object to existing scene
         scene.add.existing(this);
